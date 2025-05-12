9cea9beb376bf5005339ee04731eadd6c6d14dfa
ravina <borkarravin77@gmail.com>
2025-05-12 12:04:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Amazon Linux ARM Node.js app deployed via Jenkins & PM2!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
