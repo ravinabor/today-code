4bdd22d72789fbacf2b192d70a1ae68bf1eee276
ravinabor <121058589+ravinabor@users.noreply.github.com>
2025-05-13 14:04:35 +0530

    Update index.js
    cc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Amazon Linux ARM Node.js app deployed via Jenkins & PM2 -123 added!\n');
+  res.end('Hello from Amazon Linux ARM Node.js app deployed via Jenkins & PM2 -54321 added!\n');
 });
 
 server.listen(PORT, () => {
