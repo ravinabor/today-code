fed036ad3f2e699603042e840f2fe5176237fed6
ravinabor <121058589+ravinabor@users.noreply.github.com>
2025-05-13 15:53:20 +0530

    Update ecosystem.config.js
    saved

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'node-cicd-demo',
-    script: './index.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
+  apps: [
+    {
+      name: 'node-cicd-demo',
+      script: '/root/opt/index.js', // Absolute path is safer for services
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
     }
-  }]
+  ]
 };
