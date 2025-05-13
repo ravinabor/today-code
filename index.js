3e50816a387708020ad1327bc26bfd4b89c66cc5
ravinabor <121058589+ravinabor@users.noreply.github.com>
2025-05-13 14:28:56 +0530

    Update index.js
    Update index.js  prompt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello !\n');
+  res.end('Hello ravnaa !\n');
 });
 
 server.listen(PORT, () => {
