6da543f9de85db0293aa7538e0f910c8af11e2c3
ravinabor <121058589+ravinabor@users.noreply.github.com>
2025-06-10 12:56:55 +0530

    Update index.js
    10 june added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello ravnaa mankar borkar nagpur updated email alerts 14th may 2025!\n');
+  res.end('Hello ravnaa mankar borkar nagpur updated email alerts 10 june  2025!\n');
 });
 
 server.listen(PORT, () => {
